75d510904b1afd67aa414d1d66dc5f06a57d6eab
Nikhil <nikhilmohan775@gmail.com>
2024-04-24 19:20:43 +0530

    changed position of avatar

--- src/components/basic/navbar/loggedin-navbar.jsx
+++ src/components/basic/navbar/loggedin-navbar.jsx
@@ -134,7 +134,7 @@ export default function LoggedInNavBar() {
           </SheetContent>
         </Sheet>
         <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
-          {/* <form className="ml-auto flex-1 sm:flex-initial">
+          {/* <form >
             <div className="relative">
               <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
               <Input
@@ -144,6 +144,9 @@ export default function LoggedInNavBar() {
               />
             </div>
           </form> */}
+          <div className="ml-auto flex-1 sm:flex-initial">
+
+         
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <Button className="rounded-full" size="icon" variant="ghost">
@@ -174,7 +177,8 @@ export default function LoggedInNavBar() {
               </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
-        </div>
+       </div>
+         </div>
       </header>
    
     </>
